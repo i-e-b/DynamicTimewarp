3085718f05aaf69c0f3a12b0d060394ab5d829d1
Iain Ballard <iain.ballard@gocompare.com>
2017-09-26 13:54:10 +0100

    Codes

---
+++ DynamicTimewarp/DynamicTimewarp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Demo
+{
+    /// <summary>
+    /// A C# Implementation of https://jeremykun.com/2012/07/25/dynamic-time-warping/
+    /// Compare two sequences for similar 'shapes', ignoring specific position.
+    /// </summary>
+    public class DynamicTimewarp
+    {
+        public static int Compare(int[] seqA, int[] seqB, Func<int, int, int> costFunc)
+        {
+            // cost matrix
+            var numRows = seqA.Length;
+            var numCols = seqB.Length;
+
+            if (numRows < 1 || numCols < 1) return 0;
+            if (numRows == 1 && numCols == 1) return costFunc(seqA[0], seqB[0]);
+
+            var cost = new int[numRows, numCols];
+
+            // fill in the first row and column
+            cost[0, 0] = costFunc(seqA[0], seqB[0]);
+            for (int i = 1; i < numRows; i++)
+                cost[i, 0] = cost[i - 1, 0] + costFunc(seqA[i], seqB[0]);
+
+            for (int j = 1; j < numCols; j++)
+                cost[0, j] = cost[0,j - 1] + costFunc(seqA[0], seqB[j]);
+
+            // calculate the rest of the matrix
+            for (int i = 1; i < numRows; i++)
+            {
+                for (int j = 1; j < numCols; j++)
+                {
+                    var minChoice = Math.Min(cost[i-1,j], Math.Min(cost[i, j-1], cost[i-1,j-1]));
+                    cost[i, j] = minChoice + costFunc(seqA[i], seqB[j]);
+                }
+            }
+
+            // Write a diagnostic to the console
+            LogArray(cost);
+
+            return cost[numRows - 1, numCols - 1];
+        }
+
+        private static void LogArray(int[,] arr)
+        {
+            Console.WriteLine(new string('-', arr.GetLength(1) * 5));
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++) {
+                    Console.Write((arr[i,j]+"      ").Substring(0,5));
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ DynamicTimewarp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main()
+        {
+            var seq_A = new[] { 0, 0, 0, 3, 6, 13, 25, 22, 7, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            var seq_B = new[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 5, 12, 24, 23, 8, 3, 1, 0, 0, 0, 0, 0 };
+            var seq_C = new[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -4, -5, -12, -24, -23, -8, -3, -1, 0, 0, 0, 0, 0 };
+            var seq_D = new[] { 25, 0, -25, 0, 25, 0, -25, 0, 25, 0, -25, 0, 25, 0, -25, 0, 25, 0, -25, 0, 25, 0, -25, 0 };
+
+
+            Func<int, int, int> costFunc = (a, b) => Math.Abs(Math.Abs(a) - Math.Abs(b));
+
+            var costAA = DynamicTimewarp.Compare(seq_A, seq_A, costFunc);
+
+            var costAB = DynamicTimewarp.Compare(seq_A, seq_B, costFunc);
+            var costBC = DynamicTimewarp.Compare(seq_B, seq_C, costFunc);
+            var costAC = DynamicTimewarp.Compare(seq_A, seq_C, costFunc);
+            var costAD = DynamicTimewarp.Compare(seq_A, seq_D, costFunc);
+
+
+            Console.WriteLine("Total cost A<->A: " + costAA);
+
+            Console.WriteLine("Total cost  A->B: " + costAB);
+            Console.WriteLine("Total cost  B->C: " + costBC);
+            Console.WriteLine("Total cost  A->C: " + costAC);
+            Console.WriteLine("Total cost  A->D: " + costAD);
+
+            Console.ReadLine();
+        }
+    }
+}
